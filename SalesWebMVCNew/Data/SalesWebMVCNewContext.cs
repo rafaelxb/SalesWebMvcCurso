b5862ba3451361a46a58802743e9c7efb846e0cd
rafaelxb <rfxbarbosa@gmail.com>
2023-05-09 20:05:57 -0300

    Adicionar arquivos de projeto.

---
+++ SalesWebMVCNew/Data/SalesWebMVCNewContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SalesWebMVCNew.Models;
+
+namespace SalesWebMVCNew.Data
+{
+    public class SalesWebMVCNewContext : DbContext
+    {
+
+        public SalesWebMVCNewContext (DbContextOptions<SalesWebMVCNewContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<SalesWebMVCNew.Models.Department> Department { get; set; } = default!;
+    }
+}
