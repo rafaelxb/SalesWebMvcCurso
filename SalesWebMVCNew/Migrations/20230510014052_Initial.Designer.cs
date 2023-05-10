bdcf396394a2bbe4c11e911eaa05c8d7edde2f70
rafaelxb <rfxbarbosa@gmail.com>
2023-05-09 22:42:19 -0300

    second migration

--- SalesWebMVCNew/Migrations/20230510014052_Initial.Designer.cs
+++ SalesWebMVCNew/Migrations/20230510014052_Initial.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SalesWebMVCNew.Data;
 
@@ -10,9 +11,11 @@ using SalesWebMVCNew.Data;
 namespace SalesWebMVCNew.Migrations
 {
     [DbContext(typeof(SalesWebMVCNewContext))]
-    partial class SalesWebMVCNewContextModelSnapshot : ModelSnapshot
+    [Migration("20230510014052_Initial")]
+    partial class Initial
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
