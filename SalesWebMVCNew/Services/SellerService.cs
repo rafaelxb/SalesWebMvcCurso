710188eb9f99aa40e920f10ef5121483a85d28ea
rafaelxb <rfxbarbosa@gmail.com>
2023-05-22 21:15:26 -0300

    Simple Create form

--- SalesWebMVCNew/Services/SellerService.cs
+++ SalesWebMVCNew/Services/SellerService.cs
@@ -16,5 +16,11 @@ namespace SalesWebMVCNew.Services
         {
             return _context.Seller.ToList();
         }
+
+        public void Insert(Seller seller)
+        {
+            _context.Add(seller);
+            _context.SaveChanges();
+        }
     }
 }
